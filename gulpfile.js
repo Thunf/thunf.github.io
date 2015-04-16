08015b6c2e937ceb473acbe91537201acdc40506
Thunf <458859360@qq.com>
2015-04-16 13:34:26 +0800

    add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -8,59 +8,70 @@ var jshint = require("gulp-jshint");
 
 var cssminify = require("gulp-minify-css");
 
+var browserSync = require("browser-sync").create();
+
 
 //路径
 var paths = {
-	js :'./dev/**/*.js',
-	css:'./dev/**/*.css'
+    js :"./dev/**/*.js",
+    css:"./dev/**/*.css"
 };
 
 var pathsto = {
-	js :'./assets/js/',
-	css:'./assets/css/'
+    js :"./assets/js/",
+    css:"./assets/css/"
 };
 
-//js语法校验
-gulp.task('jshint',function(){
-	return gulp.src(paths.js)
-			   .pipe(jshint())
-			   .pipe(jshint.reporter('default'));
+// js hint
+gulp.task("jshint",function(){
+    return gulp.src(paths.js)
+               .pipe(jshint())
+               .pipe(jshint.reporter("default"));
 });
 
-//js连接、压缩、歧义化
-gulp.task('jsmini',['jshint'],function(){
-	gulp.src(paths.js)
-		.pipe(concat('main.js'))
-		.pipe(gulp.dest(pathsto.js))
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: '.min',
-			extname: '.js'
-		}))
-		.pipe(gulp.dest(pathsto.js));
+// js concat/uglify/minify
+gulp.task("jsmini",["jshint"],function(){
+    return gulp.src(paths.js)
+        .pipe(concat("main.js"))
+        .pipe(gulp.dest(pathsto.js))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(gulp.dest(pathsto.js))
+        .pipe(browserSync.reload({stream: true}));
 });
 
-//css连接、压缩
-gulp.task('cssmini',function(){
-	gulp.src(paths.css)
-		.pipe(concat('main.css'))
-		.pipe(gulp.dest(pathsto.css))
-		.pipe(cssminify())
-		.pipe(rename({
-			suffix: '.min',
-			extname: '.css'
-		}))
-		.pipe(gulp.dest(pathsto.css));
+// css concat/minify
+gulp.task("cssmini",function(){
+    return gulp.src(paths.css)
+        .pipe(concat("main.css"))
+        .pipe(gulp.dest(pathsto.css))
+        .pipe(cssminify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(gulp.dest(pathsto.css))
+        .pipe(browserSync.reload({stream: true}));
 });
 
+// browser-sync
+gulp.task("browser-sync",function(){
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("./index.html").on("change", browserSync.reload);
+});
 
-//监测文件改动
-gulp.task('watch',function(){
-	var jsw = gulp.watch(paths.js, ['jsmini']);
-	var	cssw = gulp.watch(paths.css, ['cssmini']);
+// watch
+gulp.task("watch", function(){
+    gulp.watch(paths.js, ["jsmini"]);
+    gulp.watch(paths.css, ["cssmini"]);
 });
 
 //默认方法
-gulp.task('default', ['cssmini','jsmini','watch'],function(){
-	console.log("ww");
+gulp.task("default", ["browser-sync","cssmini","jsmini","watch"],function(){
+    console.log("gulp begin to work!");
 });
